38cc017a622ba4e92d25250dc5769e578a274c7d
Cleggameg <Cleggameg@gmail.com>
2015-06-17 14:56:27 -0500

    simple login using facebook, google, and twitter

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get 'auth/:provider/callback', to: 'sessions#create'
-  get 'auth/failure', to: redirect('/')
-  get 'signout', to: 'sessions#destroy', as: 'signout'
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  # get 'auth/:provider/callback', to: 'sessions#create'
+  # get 'auth/failure', to: redirect('/')
+  # get 'signout', to: 'sessions#destroy', as: 'signout'
 
-  resources :sessions, only: [:create, :destroy]
+  # resources :sessions, only: [:create, :destroy]
   resource :home, only: [:show]
 
   root to: "home#show"
